67aa643dcfe87c56e88a1842f017e13dabd229f6
Yashodha-S452003 <yashodhayash45@gmail.com>
2026-02-16 21:37:04 +0530

    added student details

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,17 +2,17 @@ package model;
 
 public class Student {
 
-    private String name;
+    private String studentname;
     private String email;
     private int marks;
 
     public Student(String name, String email, int marks) {
-        this.name = name;
+        this.studentname = name;
         this.email = email;
         this.marks = marks;
     }
 
-    public String getName() { return name; }
+    public String getName() { return studentname; }
     public String getEmail() { return email; }
     public int getMarks() { return marks; }
 }
